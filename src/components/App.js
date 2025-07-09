4b42fc2d9908e156b1ea4a543fb51bcf49d1cdcf
Kristopher Ilich <kristopherilich@gmail.com>
2025-07-08 23:04:02 -0700

    one more pkg fix

--- src/components/App.js
+++ src/components/App.js
@@ -56,7 +56,7 @@ function App() {
     }, [])
 
   return (
-    <BrowserRouter basename="/sd-surplus">
+    <BrowserRouter>
     <ScrollToTop>
       {/* 1) Fixed nav bar at the top */}
       <NavBar 
